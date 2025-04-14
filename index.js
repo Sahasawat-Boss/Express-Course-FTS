29ef6b4857e1d4d801ee86a412e5595b6084e230
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-14 20:35:03 +0700

    a delete api

--- index.js
+++ index.js
@@ -20,18 +20,41 @@ const port = 3000
 // name
 
 const todoList = [];
+// body data { id: 'test-1', status: 'in-progress', name: 'Task-1' }
 
 app.get('/todos', (req, res) => {
     console.log(todoList);
-    res.send('Get all data:', todoList);
-})
+    res.json({
+        message: 'All Todos',
+        data: todoList
+    });
+});
 
 app.post('/todos', (req, res) => {
     console.log('body data', req.body);
     todoList.push(req.body);
     res.send(req.body);
 })
-// body data { id: 'test-1', status: 'in-progress', name: 'Task-1' }
+
+app.delete('/todos/:id', (req, res) => {
+    const todoIndex = todoList.findIndex((todo) => todo.id === req.params.id);
+
+    if (todoIndex === -1) {
+        res.status(404).send("Todo not found")
+        return
+    }
+
+    // remove and store the deleted item
+    const deletedTodo = todoList.splice(todoIndex, 1)[0];
+
+    console.log('Delete data successful:', deletedTodo);
+
+    res.json({
+        message: "Todo deleted successfully",
+        deleted: deletedTodo
+    });
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`)
